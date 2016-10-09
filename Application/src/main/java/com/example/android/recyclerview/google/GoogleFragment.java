e8b608fec63e3e04f6452feaf6474b77a82c31e5
Joaquim Puyo <joaquim.puyo@worldline.com>
2016-10-10 00:12:26 +0200

    renamed recycler view fragment

--- Application/src/main/java/com/example/android/recyclerview/google/GoogleFragment.java
+++ Application/src/main/java/com/example/android/recyclerview/google/GoogleFragment.java
@@ -60,7 +60,7 @@ public class GoogleFragment extends SampleFragmentBase {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.recycler_view_frag, container, false);
+        View rootView = inflater.inflate(R.layout.google_fragment, container, false);
         rootView.setTag(TAG);
 
         // BEGIN_INCLUDE(initializeRecyclerView)
